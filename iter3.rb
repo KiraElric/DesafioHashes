4b06b7d5705bb159366cab60532bc039d1d45bd0
KiraElric <lissette.urso@gmail.com>
2020-07-13 20:59:30 -0400

    Desafio 3

---
+++ iter3.rb
@@ -0,0 +1,27 @@
+def filter(hash)
+  new_hash = {}
+  condition = ARGV[0].to_i
+  hash.each do |k,v|
+    if v > condition
+      new_hash[k] = v
+    end
+  end
+  return new_hash.values
+end
+
+ventas = {
+  Enero: 15000,
+  Febrero: 22000,
+  Marzo: 12000,
+  Abril: 17000,
+  Mayo: 81000,
+  Junio: 13000,
+  Julio: 21000,
+  Agosto: 41200,
+  Septiembre: 25000,
+  Octubre: 21500,
+  Noviembre: 91000,
+  Diciembre: 21000
+  }
+
+filter(ventas)
